7bfb3afd79f8afba6628e038faa28f8a1fb8a46b
brutzman <brutzman@e1939eb6-e29d-498f-9c52-b90c59512e47>
2022-08-21 23:24:05 +0000

    hide layer-registration information (use annotations instead)

--- www.web3d.org/x3d/tools/X3dEdit4.0/X3dEditModuleSuite/X3dSourceFilePalette/src/org/web3d/x3d/palette/package-info.java
+++ www.web3d.org/x3d/tools/X3dEdit4.0/X3dEditModuleSuite/X3dSourceFilePalette/src/org/web3d/x3d/palette/package-info.java
@@ -1,12 +1,12 @@
-@PaletteItemRegistration(
-         paletteid = "X3dPalette",
-         category = "Items",
-         itemid = "COMMENT",
-         icon32 = "org/web3d/x3d/palette/items/resources/COMMENT32.png",
-         icon16 = "org/web3d/x3d/palette/items/resources/COMMENT32.png",
-         body = "/* new comment */",
-         name = "New Comment",
-         tooltip = "/* new comment */")
+//@PaletteItemRegistration(
+//         paletteid = "X3DPalette",
+//         category = "Items",
+//         itemid = "COMMENT",
+//         icon32 = "org/web3d/x3d/palette/items/resources/COMMENT32.png",
+//         icon16 = "org/web3d/x3d/palette/items/resources/COMMENT32.png",
+//         body = "/* new comment */",
+//         name = "New Comment",
+//         tooltip = "/* new comment */")
         
 package org.web3d.x3d.palette;
 
