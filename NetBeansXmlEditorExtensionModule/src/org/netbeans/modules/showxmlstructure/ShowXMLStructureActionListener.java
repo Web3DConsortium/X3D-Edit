2113d9650a910578e233a27549a37900a3d57fbe
tnorbraten <tnorbraten@e1939eb6-e29d-498f-9c52-b90c59512e47>
2022-08-27 19:05:13 +0000

    [Terry N.] update

--- NetBeansXmlEditorExtensionModule/src/org/netbeans/modules/showxmlstructure/ShowXMLStructureActionListener.java
+++ NetBeansXmlEditorExtensionModule/src/org/netbeans/modules/showxmlstructure/ShowXMLStructureActionListener.java
@@ -4,14 +4,7 @@
  */
 package org.netbeans.modules.showxmlstructure;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import org.openide.awt.ActionID;
-import org.openide.awt.ActionReference;
 import org.openide.awt.ActionReferences;
-import org.openide.awt.ActionRegistration;
-import org.openide.cookies.EditorCookie;
-import org.openide.util.NbBundle.Messages;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
@@ -53,47 +46,47 @@ public final class ShowXMLStructureActionListener implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent ev) {
-    // https://netbeans.apache.org/tutorials/nbm-xmleditor.html
-    
-    // "XML Structure" tab is created in Output Window for writing the list of tags:
-    InputOutput io = IOProvider.getDefault().getIO(Bundle.CTL_ShowXMLStructureActionListener(), false);
-    io.select(); //"XML Structure" tab is selected
-    try {
-        //Get the InputStream from the EditorCookie:
-        InputStream is = ((org.openide.text.CloneableEditorSupport) context).getInputStream();
-        //Use the NetBeans org.openide.xml.XMLUtil class to create a org.w3c.dom.Document:
-        Document doc =  XMLUtil.parse(new InputSource(is), true, true, null, null);
-        //Create a list of nodes, for all the elements:
-        NodeList list = doc.getElementsByTagName("*");
-        //Iterate through the list:
-        for (int i = 0; i < list.getLength(); i++) {
-            //For each node in the list, create a org.w3c.dom.Node:
-            org.w3c.dom.Node mainNode = list.item(i);
-            //Create a map for all the attributes of the org.w3c.dom.Node:
-            NamedNodeMap map = mainNode.getAttributes();
-            //Get the name of the node:
-            String nodeName = mainNode.getNodeName();
-            //Create a StringBuilder for the Attributes of the Node:
-            StringBuilder attrBuilder = new StringBuilder();
-            //Iterate through the map of attributes:
-            for (int j = 0; j < map.getLength(); j++) {
-                //Each iteration, create a new Node:
-                org.w3c.dom.Node attrNode = map.item(j);
-                //Get the name of the current Attribute:
-                String attrName = attrNode.getNodeName();
-                //Add the current Attribute to the StringBuilder:
-                attrBuilder.append("*").append(attrName).append(" ");
+        // https://netbeans.apache.org/tutorials/nbm-xmleditor.html
+
+        // "XML Structure" tab is created in Output Window for writing the list of tags:
+        InputOutput io = IOProvider.getDefault().getIO(Bundle.CTL_ShowXMLStructureActionListener(), false);
+        io.select(); //"XML Structure" tab is selected
+        try {
+            //Use the NetBeans org.openide.xml.XMLUtil class to create a org.w3c.dom.Document:
+            try ( //Get the InputStream from the EditorCookie:
+                    
+                InputStream is = ((org.openide.text.CloneableEditorSupport) context).getInputStream()) {
+                //Use the NetBeans org.openide.xml.XMLUtil class to create a org.w3c.dom.Document:
+                Document doc =  XMLUtil.parse(new InputSource(is), true, true, null, null);
+                //Create a list of nodes, for all the elements:
+                NodeList list = doc.getElementsByTagName("*");
+                //Iterate through the list:
+                for (int i = 0; i < list.getLength(); i++) {
+                    //For each node in the list, create a org.w3c.dom.Node:
+                    org.w3c.dom.Node mainNode = list.item(i);
+                    //Create a map for all the attributes of the org.w3c.dom.Node:
+                    NamedNodeMap map = mainNode.getAttributes();
+                    //Get the name of the node:
+                    String nodeName = mainNode.getNodeName();
+                    //Create a StringBuilder for the Attributes of the Node:
+                    StringBuilder attrBuilder = new StringBuilder();
+                    //Iterate through the map of attributes:
+                    for (int j = 0; j < map.getLength(); j++) {
+                        //Each iteration, create a new Node:
+                        org.w3c.dom.Node attrNode = map.item(j);
+                        //Get the name of the current Attribute:
+                        String attrName = attrNode.getNodeName();
+                        //Add the current Attribute to the StringBuilder:
+                        attrBuilder.append("*").append(attrName).append(" ");
+                    }
+                    //Print the element and its attributes to the Output window:
+                    io.getOut().println("ELEMENT: " + nodeName
+                            + " --> ATTRIBUTES: " + attrBuilder.toString());
+                }
+                //Close the InputStream:
             }
-            //Print the element and its attributes to the Output window:
-            io.getOut().println("ELEMENT: " + nodeName
-                    + " --> ATTRIBUTES: " + attrBuilder.toString());
+        } catch (SAXException | IOException ex) {
+            Exceptions.printStackTrace(ex);
         }
-        //Close the InputStream:
-        is.close();
-    } catch (SAXException ex) {
-        Exceptions.printStackTrace(ex);
-    } catch (IOException ex) {
-        Exceptions.printStackTrace(ex);
-    }
     }
 }
