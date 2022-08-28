dba57ecd9d53a3c1cdf1d8e060a7267c436ac41c
brutzman <brutzman@e1939eb6-e29d-498f-9c52-b90c59512e47>
2022-08-28 20:42:44 +0000

    fix issue 4521, Exceptions.printStackTrace

--- www.web3d.org/x3d/tools/X3dEdit4.0/X3dEditModuleSuite/X3dSourceFilePalette/src/org/web3d/x3d/palette/X3DPaletteFactory.java
+++ www.web3d.org/x3d/tools/X3dEdit4.0/X3dEditModuleSuite/X3dSourceFilePalette/src/org/web3d/x3d/palette/X3DPaletteFactory.java
@@ -1,8 +1,43 @@
+/*
+Copyright (c) 1995-2022 held by the author(s) .  All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+ * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer
+      in the documentation and/or other materials provided with the
+      distribution.
+ * Neither the names of the Naval Postgraduate School (NPS)
+      Modeling Virtual Environments and Simulation (MOVES) Institute
+      (http://www.nps.edu and https://MovesInstitute.nps.edu)
+      nor the names of its contributors may be used to endorse or
+      promote products derived from this software without specific
+      prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package org.web3d.x3d.palette;
 
 /**
  * X3D Palette for Netbeans
  * https://netbeans.apache.org/tutorials/nbm-palette-api1.html
+ * https://netbeans.apache.org/tutorials/nbm-palette-api2.html
  * @author brutzman@nps.edu
  */
 
@@ -13,16 +48,18 @@ import org.netbeans.spi.palette.DragAndDropHandler;
 import org.netbeans.spi.palette.PaletteActions;
 import org.netbeans.spi.palette.PaletteController;
 import org.netbeans.spi.palette.PaletteFactory;
+import org.openide.util.Exceptions;
 import org.openide.util.Lookup;
 import org.openide.util.datatransfer.ExTransferable;
 //          TODO Issue 4521  import org.openide.util.Exceptions;
 
 public class X3DPaletteFactory
 {
-    public  static final String X3D_PALETTE_FOLDER = "X3DPalette"; 
     private static PaletteController palette = null;
+    public  static final String X3D_PALETTE_FOLDER = "X3DPalette"; 
 
     @MimeRegistration (mimeType = "text/xml", service = PaletteController.class) // TODO add X3D MIME type registration
+
     public static PaletteController createPalette()
     {
         try {
@@ -41,13 +78,16 @@ public class X3DPaletteFactory
                     //Palette Filter:
                     null,
                     //Drag and Drop Handler:
-                    new  DragAndDropHandler(true) {
+                    new  DragAndDropHandler(true)
+                    {
                         @Override public void customize(ExTransferable et, Lookup lkp) {}
                 });
             }
-        } catch (IOException ex) {
-//          TODO Issue 4521  Exceptions.printStackTrace(ex);
-            ex.printStackTrace();
+        }
+        catch (IOException ex)
+        {
+            Exceptions.printStackTrace(ex);
+//          TODO Issue 4521  ex.printStackTrace();
         }
         return null;
     }
