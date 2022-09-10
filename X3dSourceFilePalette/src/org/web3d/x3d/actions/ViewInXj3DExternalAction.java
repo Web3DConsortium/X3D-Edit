98e65a99ed7323f0ac18b0b05554faf46e7dfb88
tnorbraten <tnorbraten@e1939eb6-e29d-498f-9c52-b90c59512e47>
2022-09-10 00:11:26 +0000

    [Terry N.] correct note

--- X3dSourceFilePalette/src/org/web3d/x3d/actions/ViewInXj3DExternalAction.java
+++ X3dSourceFilePalette/src/org/web3d/x3d/actions/ViewInXj3DExternalAction.java
@@ -47,7 +47,7 @@ import org.web3d.x3d.options.X3dOptions;
 @ActionRegistration(
            iconBase = "org/web3d/x3d/resources/xj3d.png",
         displayName = "#CTL_ViewInXj3DExternalAction",
-                lazy=true) // don't do lazy=true since iconBase no longer gets registered
+                lazy=true) // don't do lazy=false since iconBase no longer gets registered
 @ActionReferences(value={
   @ActionReference(path = "Menu/X3D-Edit/View Saved Scene", position = 60, separatorAfter = 61),
   @ActionReference(path = "Editors/model/x3d+xml/Popup/View Saved Scene", position = 61),
