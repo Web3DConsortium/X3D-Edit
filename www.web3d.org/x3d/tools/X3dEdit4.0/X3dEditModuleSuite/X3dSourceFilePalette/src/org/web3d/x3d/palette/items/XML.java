50e42f90c3ee59640e03621fe6e8610c9044b680
brutzman <brutzman@e1939eb6-e29d-498f-9c52-b90c59512e47>
2022-09-18 08:14:49 +0000

    add XML customizer

--- www.web3d.org/x3d/tools/X3dEdit4.0/X3dEditModuleSuite/X3dSourceFilePalette/src/org/web3d/x3d/palette/items/XML.java
+++ www.web3d.org/x3d/tools/X3dEdit4.0/X3dEditModuleSuite/X3dSourceFilePalette/src/org/web3d/x3d/palette/items/XML.java
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 1995-2021 held by the author(s) .  All rights reserved.
+Copyright (c) 1995-2022 held by the author(s) .  All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions
@@ -42,8 +42,8 @@ import static org.web3d.x3d.types.X3DSchemaData.*;
 @PaletteItemRegistration
 (
     paletteid = "X3DPalette",
-    category = "1. X3D Structure and Metadata",
-    itemid = "0_XML",
+    category = "1. X3D Model Structure and Metadata",
+    itemid = "1_XML",
     icon32 = "org/web3d/x3d/palette/items/resources/XML32.png",
     icon16 = "org/web3d/x3d/palette/items/resources/XML16.png",
     body = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n",
@@ -71,8 +71,6 @@ import static org.web3d.x3d.types.X3DSchemaData.*;
  */
 public class XML extends SceneGraphStructureNodeType
 {
-  private String version;
-  
   public XML()
   {
   }
@@ -80,124 +78,30 @@ public class XML extends SceneGraphStructureNodeType
   @Override
   public String getElementName()
   {
-    return DOCTYPE_ELNAME;
+    return XML_ELNAME;
   }
 
   @Override
   public Class<? extends BaseCustomizer> getCustomizer()
   {
-    return DOCTYPECustomizer.class;
+    return XMLCustomizer.class;
   }
 
   @Override
   public void initialize()
   {
-    version   = DOCTYPE_ATTR_VERSION_DFLT;
+    // no modification
   }
 
   @Override
   public void initializeFromJdom(org.jdom.Element root, JTextComponent comp)
   {
     super.initializeFromJdom(root, comp);
-
-    org.jdom.Attribute attr = root.getAttribute(DOCTYPE_ATTR_VERSION_NAME);
-    if (attr != null)
-      version = attr.getValue();
   }
 
   @Override
   public String createAttributes()
   {
-    StringBuilder sb = new StringBuilder();
-    sb.append("<!DOCTYPE X3D PUBLIC \"ISO//Web3D//DTD X3D ");
-    sb.append(version);
-    sb.append("//EN\" \"https://www.web3d.org/specifications/x3d-");
-    sb.append(version);
-    sb.append(".dtd\">");
-    return sb.toString();
-  }
-
-  private final String xmlHdrRegEx         = "<\\?xml version=\"1.0\" encoding=\"UTF-8\"\\?>";
-  private final String xmlHdr              = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
-  private final String dtdHdr              = "<!DOCTYPE X3D ";
-  private final String dtdHdr1             = "PUBLIC ";
-  private final String dtdFtr              = ">";
-  
-  private final String dtdNameElement0     = "\"ISO//Web3D//DTD X3D "; //3.1
-  private final String dtdNameElement1     = "//EN\"";
-  private final String dtdLocationElement0 = "\"https://www.web3d.org/specifications/x3d-"; // version is inserted here
-  private final String dtdLocationElement1 = ".dtd\"";
-  
-  private final String regexPattern =
-      xmlHdrRegEx+
-      "\\s*" + // whitespace including cr/lf, zero or more
-      dtdHdr+
-      ".*"   + // any chars, 0 or more, PUBLIC/SYSTEM fits here
-      "\""   + // quote
-      ".*"   + // any chars, 0 or more
-      "\""   + // quote
-      "\\s*" + // whitespace including cr/lf, zero or more
-      "\""   + // quote
-      ".*"   + // any chars, 0 or more
-      "\""   + // quote
-      "\\s*" + // whitespace including cr/lf, zero or more
-      dtdFtr;
-  
-  /**
-   * This gets called after a good insert when things have settled down.  Here we try to make sure the DTD spec is the same as our chosen version.
-   * The schema reference gets changed above, so try for the xml and dtd headers here.
-   * @param jTextComponent 
-   */
-  @Override
-  public void postInsert(JTextComponent jTextComponent)
-  {
-    String completeText = jTextComponent.getText();
-	
-	// special handling to remove duplicate DOCTYPE
-	if (completeText.indexOf("<!DOCTYPE") !=     completeText.lastIndexOf("<!DOCTYPE"))
-		completeText = completeText.substring(0, completeText.indexOf    ("<!DOCTYPE")) + // snip
-				       completeText.substring(   completeText.lastIndexOf("<!DOCTYPE"));
-
-    String s = completeText;
-    int size = completeText.length();
-    
-    if(size > 255) {
-      // operate on a portion of the whole thing
-      s = s.substring(0, 256);
-      size = 255;
-    }
-    
-    String newHeader = xmlHdr+linesep+dtdHdr+dtdHdr1;  // first part of new header
-    
-    version = getVersion();
-    if (version.equals("3.0") || version.equals("3.1") || version.equals("3.2") || version.equals("3.3") ||
-	    version.equals("4.0") || version.equals("4.1"))
-      ; // nop
-    else {
-      System.err.println("Bad X3D version='" + version + "' found by DOCTYPE.java: must be 3.0, 3.1, 3.2, 3.3, 4.0 or 4.1");
-      return;
-    }
-
-    newHeader = newHeader+dtdNameElement0+version+dtdNameElement1 + " " + dtdLocationElement0+version+dtdLocationElement1+dtdFtr;
-    
-    // Quickly find out if we matched; if not, oddball existing header maybe...leave it alone
-    String[] sa = s.split(regexPattern);
-    if(sa == null || sa.length<2) {
-      System.err.println("No DTD header match found by DOCTYPE.java");
-      return;
-    }
-    
-    // Do the replace
-    jTextComponent.setText(completeText.replaceFirst(regexPattern, newHeader));
-  }
-
-  public String getVersion()
-  {
-    return version;
-  }
-
-  public void setVersion(String newVersion)
-  {
-    version = newVersion;
+      return ""; // nada
   }
 }
