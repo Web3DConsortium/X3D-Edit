4fe49ecc11d0332e8d215a6a53e9ae6100382e5c
brutzman <brutzman@e1939eb6-e29d-498f-9c52-b90c59512e47>
2022-10-31 01:45:16 +0000

    sort and improve menu names

--- X3dSourceFilePalette/src/org/web3d/x3d/config/MyViewBrowserAction.java
+++ X3dSourceFilePalette/src/org/web3d/x3d/config/MyViewBrowserAction.java
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 1995-2021 held by the author(s) .  All rights reserved.
+Copyright (c) 1995-2022 held by the author(s) .  All rights reserved.
  
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions
@@ -36,23 +36,23 @@ package org.web3d.x3d.config;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import org.openide.awt.StatusDisplayer;
-import org.openide.awt.HtmlBrowser;
-import org.openide.util.Exceptions;
-import org.openide.util.NbBundle;
 import java.net.URL;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionReferences;
 import org.openide.awt.ActionRegistration;
+import org.openide.awt.HtmlBrowser;
+import org.openide.awt.StatusDisplayer;
+import org.openide.util.Exceptions;
+import org.openide.util.NbBundle;
 
 @ActionID(id = "org.web3d.x3d.x3dedit.config.MyViewBrowserAction", category = "Tools")
-@ActionRegistration(// TODO iconBase = "org/web3d/x3d/resources/X3Dicon16.png",
-                    displayName = "#CTL_MyViewBrowserAction", // "X3D-Edit home page"
+@ActionRegistration(   iconBase = "org/web3d/x3d/resources/X3Dicon16.png",
+                    displayName = "#CTL_MyViewBrowserAction", // "X3D-Edit Home"
                             lazy=true)
 @ActionReferences(value = {
-  @ActionReference(path = "Menu/X3D-Edit/X3D-Edit Home, Preferences", position = 1004), // see layer.xml
-  @ActionReference(path = "Editors/model/x3d+xml/Popup/X3D-Edit Home, Preferences", position = 1004)
+  @ActionReference(path = "Menu/X3D-Edit/X3D-Edit configuration", position = 300), // see layer.xml
+  @ActionReference(path = "Editors/model/x3d+xml/Popup/X3D-Edit configuration", position = 300)
 })
 
 public final class MyViewBrowserAction implements ActionListener
