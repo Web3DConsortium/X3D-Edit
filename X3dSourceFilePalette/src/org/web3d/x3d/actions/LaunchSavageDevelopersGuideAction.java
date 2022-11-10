ab9552be80c18b6c93aa6ed5288a29f6665d514f
brutzman <brutzman@e1939eb6-e29d-498f-9c52-b90c59512e47>
2022-11-10 21:46:17 +0000

    add Savage Developers Guide, master and DIS

---
+++ X3dSourceFilePalette/src/org/web3d/x3d/actions/LaunchSavageDevelopersGuideAction.java
@@ -0,0 +1,92 @@
+/*
+Copyright (c) 1995-2022 held by the author(s) .  All rights reserved.
+ 
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+ 
+ * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer
+      in the documentation and/or other materials provided with the
+      distribution.
+ * Neither the names of the Naval Postgraduate School (NPS)
+      Modeling Virtual Environments and Simulation (MOVES) Institute
+      (https://www.nps.edu and https://MovesInstitute.nps.edu)
+      nor the names of its contributors may be used to endorse or
+      promote products derived from this software without specific
+      prior written permission.
+ 
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.web3d.x3d.actions;
+
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionReferences;
+import org.openide.awt.ActionRegistration;
+import org.openide.util.HelpCtx;
+import org.openide.util.NbBundle;
+import org.openide.util.actions.CallableSystemAction;
+import static org.web3d.x3d.actions.BaseViewAction.SAVAGE_DEVELOPERS_GUIDE;
+
+@ActionID(id = "org.web3d.x3d.actions.LaunchSavageDevelopersGuideAction", category = "Tools")
+@ActionRegistration(   iconBase = "org/web3d/x3d/palette/items/resources/SavageLogoIcon.13x16.png",
+                    displayName = "#CTL_LaunchSavageDevelopersGuideAction",
+                            lazy=true)
+@ActionReferences( value = {
+  @ActionReference(path = "Menu/X3D-Edit/X3D-Edit Configuration", position = 150),
+  @ActionReference(path = "Editors/model/x3d+xml/Popup/X3D-Edit Configuration", position = 150),
+})
+
+public final class LaunchSavageDevelopersGuideAction extends CallableSystemAction
+{
+  @Override
+  public void performAction()
+  {
+    // looking for javahelp
+    // Help hlp = Lookup.getDefault().lookup(org.netbeans.api.javahelp.Help.class);
+    // hlp.showHelp(HelpCtx.findHelp(this));
+      
+    LaunchX3dExamplesAction.sendBrowserTo(SAVAGE_DEVELOPERS_GUIDE);
+  }
+
+  @Override
+  public String getName()
+  {
+    return NbBundle.getMessage(getClass(), "CTL_LaunchSavageDevelopersGuideAction");
+  }
+
+  @Override
+  protected void initialize()
+  {
+    super.initialize();
+    // see org.openide.util.actions.SystemAction.iconResource() Javadoc for more details
+    putValue("noIconInMenu", Boolean.TRUE);
+  }
+
+  @Override
+  public HelpCtx getHelpCtx()
+  {
+    return new HelpCtx("X3dResourcesExamples");
+  }
+
+  @Override
+  protected boolean asynchronous()
+  {
+    return false;
+  }
+}
