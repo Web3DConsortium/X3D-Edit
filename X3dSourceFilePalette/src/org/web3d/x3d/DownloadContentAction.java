b34b50c00217655774851a51ccb21acfb39bb18c
brutzman <brutzman@e1939eb6-e29d-498f-9c52-b90c59512e47>
2022-11-13 02:49:33 +0000

    fix imports

--- X3dSourceFilePalette/src/org/web3d/x3d/DownloadContentAction.java
+++ X3dSourceFilePalette/src/org/web3d/x3d/DownloadContentAction.java
@@ -35,20 +35,13 @@ POSSIBILITY OF SUCH DAMAGE.
 package org.web3d.x3d;
 
 import java.awt.BorderLayout;
-import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import javax.imageio.ImageIO;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
-import javax.swing.Icon;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -59,7 +52,6 @@ import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionReferences;
 import org.openide.awt.ActionRegistration;
-import org.openide.util.Exceptions;
 import org.openide.util.HelpCtx;
 import org.openide.util.ImageUtilities;
 import org.openide.util.NbBundle;
