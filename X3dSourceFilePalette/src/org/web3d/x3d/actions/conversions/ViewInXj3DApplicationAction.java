fcd35a81192314ab087734609f245dec19149885
brutzman <brutzman@e1939eb6-e29d-498f-9c52-b90c59512e47>
2022-11-23 07:37:36 +0000

    dialog size

--- X3dSourceFilePalette/src/org/web3d/x3d/actions/conversions/ViewInXj3DApplicationAction.java
+++ X3dSourceFilePalette/src/org/web3d/x3d/actions/conversions/ViewInXj3DApplicationAction.java
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 1995-2021 held by the author(s) .  All rights reserved.
+* Copyright (c) 1995-2022 held by the author(s) .  All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without
 * modification, are permitted provided that the following conditions
@@ -177,10 +177,12 @@ public final class ViewInXj3DApplicationAction extends CallableSystemAction
       p.setBorder(new EmptyBorder(10, 10, 0, 10));
       p.add(new JLabel(BaseConversionsAction.Nb_Select_files_to_process), BorderLayout.NORTH);
       p.add(new JScrollPane(list), BorderLayout.CENTER);
-      DialogDescriptor dd = new DialogDescriptor(p, BaseConversionsAction.Nb_Select_X3D_Files);
-      Dialog dial = DialogDisplayer.getDefault().createDialog(dd);
-      dial.setVisible(true);
-      if (dd.getValue().equals(DialogDescriptor.CANCEL_OPTION))
+      DialogDescriptor dialogDescriptor = new DialogDescriptor(p, BaseConversionsAction.Nb_Select_X3D_Files);
+      Dialog dialog = DialogDisplayer.getDefault().createDialog(dialogDescriptor);
+      dialog.setSize(300, 200);
+      // TODO set single selection, i.e. setMultipSelectionEnables(false)
+      dialog.setVisible(true);
+      if (dialogDescriptor.getValue().equals(DialogDescriptor.CANCEL_OPTION))
           return;
       
       List<String>  selectedFns = list.getSelectedValuesList();
