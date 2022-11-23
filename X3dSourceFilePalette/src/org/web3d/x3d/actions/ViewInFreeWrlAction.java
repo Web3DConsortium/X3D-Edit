80c7dfcc2001fec766e37196d3619825feea0c76
brutzman <brutzman@e1939eb6-e29d-498f-9c52-b90c59512e47>
2022-11-23 03:05:07 +0000

    freewrl icon image, hacked

--- X3dSourceFilePalette/src/org/web3d/x3d/actions/ViewInFreeWrlAction.java
+++ X3dSourceFilePalette/src/org/web3d/x3d/actions/ViewInFreeWrlAction.java
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 1995-2021 held by the author(s) .  All rights reserved.
+Copyright (c) 1995-2022 held by the author(s) .  All rights reserved.
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions
 are met:
@@ -50,7 +50,8 @@ import org.openide.util.NbBundle;
 import org.web3d.x3d.options.X3dOptions;
 
 @ActionID(id = "org.web3d.x3d.actions.ViewInFreeWrlAction", category = "View")
-@ActionRegistration(displayName = "#CTL_ViewInFreeWrlAction", lazy=true)
+@ActionRegistration(   iconBase = "org/web3d/x3d/resources/FreewrlWelcome32x32.png",
+                    displayName = "#CTL_ViewInFreeWrlAction", lazy=true)
 @ActionReferences(value={
   @ActionReference(path = "Editors/model/x3d+xml/Popup/Show Saved Model", position = 121),
   @ActionReference(path = "Menu/X3D-Edit/Show Saved Model", position = 121)
