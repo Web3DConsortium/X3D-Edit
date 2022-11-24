5400ebe7da100857c42157d7aa047da452548caf
brutzman <brutzman@e1939eb6-e29d-498f-9c52-b90c59512e47>
2022-11-24 17:56:44 +0000

    remove whitespace from license prose

--- X3dSourceFilePalette/src/org/web3d/x3d/DownloadX3dExamplesArchivesAction.java
+++ X3dSourceFilePalette/src/org/web3d/x3d/DownloadX3dExamplesArchivesAction.java
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 1995-2022 held by the author(s) .  All rights reserved.
+Copyright (c) 1995-2022 held by the author(s).  All rights reserved.
  
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions
@@ -68,7 +68,7 @@ import org.openide.util.actions.CallableSystemAction;
 })
 
 @SuppressWarnings("serial")
-public final class DownloadContentAction extends CallableSystemAction
+public final class DownloadX3dExamplesArchivesAction extends CallableSystemAction
 {
   //private static Dialog dlg;
   //private static DialogDescriptor descriptor;
@@ -206,7 +206,7 @@ public final class DownloadContentAction extends CallableSystemAction
   @Override
   public String getName()
   {
-    return NbBundle.getMessage(DownloadContentAction.class, "CTL_DownloadContentAction");
+    return NbBundle.getMessage(DownloadX3dExamplesArchivesAction.class, "CTL_DownloadContentAction");
   }
 
   @Override
