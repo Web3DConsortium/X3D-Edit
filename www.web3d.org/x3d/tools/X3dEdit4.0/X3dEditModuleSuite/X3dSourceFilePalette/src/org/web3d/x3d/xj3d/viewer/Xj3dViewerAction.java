c61a61d5363a946d711b925719d94ea2e50b45be
brutzman <brutzman@e1939eb6-e29d-498f-9c52-b90c59512e47>
2022-12-05 05:46:44 +0000

    fix imports

--- www.web3d.org/x3d/tools/X3dEdit4.0/X3dEditModuleSuite/X3dSourceFilePalette/src/org/web3d/x3d/xj3d/viewer/Xj3dViewerAction.java
+++ www.web3d.org/x3d/tools/X3dEdit4.0/X3dEditModuleSuite/X3dSourceFilePalette/src/org/web3d/x3d/xj3d/viewer/Xj3dViewerAction.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1995-2021 held by the author(s).  All rights reserved.
+ * Copyright (c) 1995-2022 held by the author(s).  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -35,12 +35,10 @@ package org.web3d.x3d.xj3d.viewer;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionReferences;
 import org.openide.awt.ActionRegistration;
-
 import org.openide.util.NbBundle;
 
 /**
