60dc02af9d1c03aa2f02c4db40258b63c3b71d95
brutzman <brutzman@e1939eb6-e29d-498f-9c52-b90c59512e47>
2022-12-11 20:46:40 +0000

    constant rename

--- X3dSourceFilePalette/src/org/web3d/x3d/palette/items/BaseCustomizer.java
+++ X3dSourceFilePalette/src/org/web3d/x3d/palette/items/BaseCustomizer.java
@@ -87,10 +87,10 @@ import static org.web3d.x3d.types.X3DSchemaData.XML_ELNAME;
 @SuppressWarnings("serial")
 public abstract class BaseCustomizer extends JPanel
 {
-    /**
-     * tooltip for mailto Report buttons
-     */
-    public  static final String MAILTO_MAILTO_TOOLTIP = "Send email issue report: please describe issue, give example .x3d excerpt or attach a snapshot";
+  /**
+   * tooltip for mailto Report buttons
+   */
+  public  static final String MAILTO_TOOLTIP = "Send email issue report: please describe issue, give example .x3d excerpt or attach a snapshot";
   private DialogDescriptor buttonsDialogDescriptor, exitDescriptor = null;
   private Dialog buttonsDialogDisplayer, exitDialog = null;
   private boolean dropOK;
@@ -364,7 +364,7 @@ public abstract class BaseCustomizer extends JPanel
     // null pointer can happen during a unit test ?!  perhaps artifact of prior javahelp dependency...
     if (reportButton != null)
     {
-        reportButton.setToolTipText(MAILTO_MAILTO_TOOLTIP);
+        reportButton.setToolTipText(MAILTO_TOOLTIP);
         reportButton.addActionListener(emailReportActionListener);
         reportButton.setVisible(true);
         reportButton.setHorizontalAlignment(SwingConstants.LEFT);
