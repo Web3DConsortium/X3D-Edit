c101a8bafb8e4e3daf6c01b3dcaf6fcab9b73c69
brutzman <brutzman@e1939eb6-e29d-498f-9c52-b90c59512e47>
2022-12-18 23:00:13 +0000

    size adjustments

--- www.web3d.org/x3d/tools/X3dEdit4.0/X3dEditModuleSuite/X3dSourceFilePalette/src/org/web3d/x3d/palette/items/HANIMSITECustomizer.java
+++ www.web3d.org/x3d/tools/X3dEdit4.0/X3dEditModuleSuite/X3dSourceFilePalette/src/org/web3d/x3d/palette/items/HANIMSITECustomizer.java
@@ -203,7 +203,7 @@ public class HANIMSITECustomizer extends BaseCustomizer
         nodeHintPanel = new javax.swing.JPanel();
         hintLabel = new javax.swing.JLabel();
 
-        setPreferredSize(new java.awt.Dimension(660, 440));
+        setPreferredSize(new java.awt.Dimension(640, 460));
         setLayout(new java.awt.GridBagLayout());
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
