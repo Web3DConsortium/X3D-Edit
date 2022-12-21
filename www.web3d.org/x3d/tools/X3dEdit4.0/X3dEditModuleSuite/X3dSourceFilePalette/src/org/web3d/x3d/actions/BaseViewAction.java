566734767a5f7a5dbb7b464328b0cad5a7978123
brutzman <brutzman@e1939eb6-e29d-498f-9c52-b90c59512e47>
2022-12-21 04:22:52 +0000

    SISO DIS/RPR FOM Product Support Group

--- www.web3d.org/x3d/tools/X3dEdit4.0/X3dEditModuleSuite/X3dSourceFilePalette/src/org/web3d/x3d/actions/BaseViewAction.java
+++ www.web3d.org/x3d/tools/X3dEdit4.0/X3dEditModuleSuite/X3dSourceFilePalette/src/org/web3d/x3d/actions/BaseViewAction.java
@@ -86,6 +86,7 @@ abstract public class BaseViewAction extends CallableSystemAction
   public final static String X3D_SCHEMATRON                         ="https://www.web3d.org/x3d/tools/schematron/X3dSchematron.html";
   public final static String X3D_X3DUOM                             ="https://www.web3d.org/specifications/X3DUOM.html";
   public final static String SAVAGE_DEVELOPERS_GUIDE                ="https://savage.nps.edu/Savage/developers.html";
+  public final static String SISO_DIS_RPR_FOM_PRODUCT_SUPPORT_GROUP ="https://www.sisostds.org/StandardsActivities/SupportGroups/DISRPRFOMPSG.aspx";
   
   public final static String WEB3D_CONSORTIUM_HOME                  ="https://web3D.org";
   public final static String WEB3D_CONSORTIUM_JOIN                  ="https://web3D.org/join";
