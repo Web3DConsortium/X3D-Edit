910ec69800eb3bf84717d9d95e41076a0524ac26
brutzman <brutzman@e1939eb6-e29d-498f-9c52-b90c59512e47>
2023-01-05 19:51:02 +0000

    x3d-public Mailing List

--- X3dSourceFilePalette/src/org/web3d/x3d/actions/LaunchX3dPublicMailingListAction.java
+++ X3dSourceFilePalette/src/org/web3d/x3d/actions/LaunchX3dPublicMailingListAction.java
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 1995-2022 held by the author(s).  All rights reserved.
+Copyright (c) 1995-2023 held by the author(s).  All rights reserved.
  
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions
@@ -34,6 +34,8 @@ POSSIBILITY OF SUCH DAMAGE.
 
 package org.web3d.x3d.actions;
 
+import java.awt.Desktop;
+import java.net.URI;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionReferences;
@@ -41,42 +43,48 @@ import org.openide.awt.ActionRegistration;
 import org.openide.util.HelpCtx;
 import org.openide.util.NbBundle;
 import org.openide.util.actions.CallableSystemAction;
-import static org.web3d.x3d.actions.BaseViewAction.X3D_ONTOLOGY;
-import static org.web3d.x3d.actions.BaseViewAction.X3D_ONTOLOGY_DOCUMENTATION;
+import static org.web3d.x3d.actions.BaseViewAction.X3D_PUBLIC_MAILING_LIST;
 
-/**
- * Launch HAnim2 standard
- * @author brutzman
- */
-    @ActionID(id = "org.web3d.x3d.actions.LaunchX3dOntologyPageAction", category = "X3D-Edit")
-@ActionRegistration(   iconBase = "org/web3d/x3d/resources/sw-cube_28x32.png",
-                    displayName = "#CTL_LaunchX3dOntologyPageAction",
+@ActionID(id = "org.web3d.x3d.actions.LaunchX3dPublicMailingListAction", category = "X3D-Edit")
+@ActionRegistration(   iconBase = "org/web3d/x3d/resources/web3d16.png",
+                    displayName = "#CTL_LaunchX3dPublicMailingListAction", 
                             lazy=true)
-@ActionReferences( value = {
-  @ActionReference(path = "Menu/&X3D-Edit/X3D &Work in Progress", position = 600),
-  @ActionReference(path = "Editors/model/x3d+xml/Popup/X3D &Work in Progress", position = 600)
+@ActionReferences(value = {
+  @ActionReference(path = "Menu/&X3D-Edit/X3D &Work in Progress", position = 100),
+  @ActionReference(path = "Editors/model/x3d+xml/Popup/X3D &Work in Progress", position = 100)
 })
 
-public final class LaunchX3dOntologyPageAction extends CallableSystemAction
+@SuppressWarnings("serial")
+public final class LaunchX3dPublicMailingListAction extends CallableSystemAction
 {
   @Override
   public void performAction()
   {
-    // looking for javahelp
-    // Help hlp = Lookup.getDefault().lookup(org.netbeans.api.javahelp.Help.class);
-    // hlp.showHelp(HelpCtx.findHelp(this));
+    sendBrowserTo(X3D_PUBLIC_MAILING_LIST);
+  }
+  
+  protected static void sendBrowserTo(String urlString)
+  {
+     try {
+       showInBrowser(urlString);
+     }
+     catch(Exception e) {
+       System.err.println("Trying to display "+urlString+" in HtmlBrowser: "+e.getLocalizedMessage());
+     }    
+  }
+  protected static void showInBrowser(String urlString) throws Exception
+  {
+    // HtmlBrowser.URLDisplayer.getDefault().showURL(new URL(urlString));
       
-    LaunchX3dExamplesAction.sendBrowserTo(X3D_ONTOLOGY_DOCUMENTATION);
-    
-    LaunchX3dExamplesAction.pauseSeconds(1); // give user time to notice first page, then second
-    
-    LaunchX3dExamplesAction.sendBrowserTo(X3D_ONTOLOGY);
+    // https://stackoverflow.com/questions/5226212/how-to-open-the-default-webbrowser-using-java
+    if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE))
+        Desktop.getDesktop().browse(new URI(urlString));
   }
 
   @Override
   public String getName()
   {
-    return NbBundle.getMessage(getClass(), "CTL_LaunchX3dOntologyPageAction");
+    return NbBundle.getMessage(getClass(), "CTL_LaunchX3dPublicMailingListAction");
   }
 
   @Override
@@ -90,7 +98,7 @@ public final class LaunchX3dOntologyPageAction extends CallableSystemAction
   @Override
   public HelpCtx getHelpCtx()
   {
-    return new HelpCtx("X3dOntologyPageAction");
+    return new HelpCtx("Web3DConsortiumMailingLists");
   }
 
   @Override
