acdab54cba3694c5359cf4884188dc93c67249b6
brutzman <brutzman@e1939eb6-e29d-498f-9c52-b90c59512e47>
2023-01-30 06:54:31 +0000

    fix url

--- X3dSourceFilePalette/src/org/web3d/x3d/actions/BaseViewAction.java
+++ X3dSourceFilePalette/src/org/web3d/x3d/actions/BaseViewAction.java
@@ -110,7 +110,7 @@ abstract public class BaseViewAction extends CallableSystemAction
   public final static String HANIM2_STANDARD                        ="https://www.web3d.org/documents/specifications/19774/V2.0/index.html";
   public final static String X3D4_ARCHITECTURE_STANDARD_CD1         ="https://www.web3d.org/specifications/X3Dv4Draft/ISO-IEC19775-1v4-CD1/Part01/Architecture.html";
   public final static String X3D4_ARCHITECTURE_STANDARD_DIS         ="https://www.web3d.org/specifications/X3Dv4Draft/ISO-IEC19775-1v4-DIS/Part01/Architecture.html";
-  public final static String X3D4_HTML_AUTHORING_GUIDELINES         =X3D4_ARCHITECTURE_STANDARD_DIS.replace("Architecture.html", "htmlGuildelines.html");
+  public final static String X3D4_HTML_AUTHORING_GUIDELINES         =X3D4_ARCHITECTURE_STANDARD_DIS.replace("Architecture.html", "htmlGuidelines.html");
   public final static String X3D_XML_ENCODING_STANDARD              ="https://www.web3d.org/documents/specifications/19776-1/V3.3/Part01/X3D_XML.html";
   public final static String X3D_CLASSICVRML_ENCODING_STANDARD      ="https://www.web3d.org/documents/specifications/19776-2/V3.3/Part02/X3D_ClassicVRML.html";
   public final static String X3D_COMPRESSED_BINARY_ENCODING_STANDARD="https://www.web3d.org/documents/specifications/19776-3/V3.3/Part03/X3D_Binary.html";
