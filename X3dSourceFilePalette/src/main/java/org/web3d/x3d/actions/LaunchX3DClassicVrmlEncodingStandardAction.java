7cbe6a5e8879e2b1f20bb27d2925c1f51b28afd8
brutzman <brutzman@nps.edu>
2024-04-21 20:08:40 -0700

    Mantis 819

--- X3dSourceFilePalette/src/main/java/org/web3d/x3d/actions/LaunchX3DClassicVrmlEncodingStandardAction.java
+++ X3dSourceFilePalette/src/main/java/org/web3d/x3d/actions/LaunchX3DClassicVrmlEncodingStandardAction.java
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 1995-2022 held by the author(s).  All rights reserved.
+Copyright (c) 1995-2024 held by the author(s).  All rights reserved.
  
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions
@@ -44,7 +44,7 @@ import org.openide.util.actions.CallableSystemAction;
 import static org.web3d.x3d.actions.BaseViewAction.X3D_CLASSICVRML_ENCODING_STANDARD;
 
 @ActionID(id = "org.web3d.x3d.actions.LaunchX3DClassicVrmlEncodingStandardAction", category = "X3D-Edit")
-@ActionRegistration(   iconBase = "org/web3d/x3d/resources/ISO_favicon32x32.png",
+@ActionRegistration(   iconBase = "org/web3d/x3d/resources/Web3dIcon32x32.png",
                     displayName = "#CTL_LaunchX3DClassicVrmlEncodingStandardAction",
                             lazy=true)
 @ActionReferences(value = {
