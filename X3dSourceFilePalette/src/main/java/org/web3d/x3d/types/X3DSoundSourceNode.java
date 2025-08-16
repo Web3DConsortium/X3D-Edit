796fdebfac3848cc94d6cbb48682f35d40bbc178
brutzman <don.brutzman@gmail.com>
2025-08-16 12:59:23 -0700

    refactor/move mislocated fields from abstract type to node

--- X3dSourceFilePalette/src/main/java/org/web3d/x3d/types/X3DSoundSourceNode.java
+++ X3dSourceFilePalette/src/main/java/org/web3d/x3d/types/X3DSoundSourceNode.java
@@ -40,7 +40,7 @@ POSSIBILITY OF SUCH DAMAGE.
 
 package org.web3d.x3d.types;
 
-import org.web3d.x3d.types.X3DPrimitiveTypes.SFDouble;
+//import org.web3d.x3d.types.X3DPrimitiveTypes.SFDouble;
 import org.web3d.x3d.types.X3DPrimitiveTypes.SFFloat;
 
 /**
@@ -54,11 +54,7 @@ import org.web3d.x3d.types.X3DPrimitiveTypes.SFFloat;
 public abstract class X3DSoundSourceNode extends X3DTimeDependentNode
 {
     protected SFFloat  gain, gainDefault;
-    protected boolean  load, loadDefault;
-    protected SFDouble autoRefresh, autoRefreshDefault; // SFTime
-    protected SFDouble autoRefreshTimeLimit, autoRefreshTimeLimitDefault; // SFTime
-    protected String[] urls, urlsDefault;
-
+    
     @Override
     public String getDefaultContainerField()
     {
@@ -77,54 +73,5 @@ public abstract class X3DSoundSourceNode extends X3DTimeDependentNode
      */
     public void setGain(String newGain) {
         gain = new SFFloat(newGain, null, null);
-    }
-
-    public boolean isLoad()
-    {
-      return load;
-    }
-
-    public void setLoad(boolean newLoad)
-    {
-      load = newLoad;
-    }
-
-    /**
-     * @param newAutoRefresh the autoRefresh to set
-     */
-    public void setAutoRefresh(String newAutoRefresh) {
-        autoRefresh = new SFDouble(newAutoRefresh, 0.0, null);
-    }
-    /**
-     * @return the autoRefresh
-     */
-    public String getAutoRefresh() {
-        return autoRefresh.toString();
-    }
-
-    /**
-     * @param newAutoRefreshTimeLimit the autoRefreshTimeLimit to set
-     */
-    public void setAutoRefreshTimeLimit(String newAutoRefreshTimeLimit) {
-        autoRefreshTimeLimit = new SFDouble(newAutoRefreshTimeLimit, 0.0, null);
-    }
-    /**
-     * @return the autoRefreshTimeLimit
-     */
-    public String getAutoRefreshTimeLimit() {
-        return autoRefreshTimeLimit.toString();
-    }
-
-    public String[] getUrls()
-    {
-      String[] urlArray = new String[urls.length];
-      System.arraycopy(urls, 0, urlArray, 0, urls.length);
-      return urlArray;
-    }
-
-    public void setUrls(String[] urlArray)
-    {
-      urls = new String[urlArray.length];
-      System.arraycopy(urlArray, 0, this.urls, 0, urlArray.length);
     }
 }
