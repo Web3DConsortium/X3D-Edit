e6297d90b30b8187d0828e3665998d7e3f5d703d
brutzman <don.brutzman@gmail.com>
2025-08-15 22:04:09 -0700

    add abstract type

---
+++ X3dSourceFilePalette/src/main/java/org/web3d/x3d/types/X3DSoundChannelNode.java
@@ -0,0 +1,116 @@
+/*
+Copyright (c) 1995-2025 held by the author(s).  All rights reserved.
+ 
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+ 
+ * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer
+      in the documentation and/or other materials provided with the
+      distribution.
+ * Neither the names of the Naval Postgraduate School (NPS)
+      Modeling Virtual Environments and Simulation (MOVES) Institute
+      (https://www.nps.edu and https://MovesInstitute.nps.edu)
+      nor the names of its contributors may be used to endorse or
+      promote products derived from this software without specific
+      prior written permission.
+ 
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+
+package org.web3d.x3d.types;
+
+import org.web3d.x3d.types.X3DPrimitiveTypes.SFDouble;
+import org.web3d.x3d.types.X3DPrimitiveTypes.SFFloat;
+
+/**
+ * X3DSoundProcessingNode.java
+ * 
+ * Base node type for all sound processing nodes, which are used to enhance audio with filtering, delaying, changing gain, etc.
+ *
+ * @author Don Brutzman
+ * @version $Id$
+ */
+public abstract class X3DSoundChannelNode extends X3DSoundNode
+{
+    protected String   channelCountMode, channelCountModeDefault;
+    protected String   channelInterpretation, channelInterpretationDefault;   
+    protected SFFloat  gain, gainDefault;
+    protected SFDouble tailTime, tailTimeDefault;
+
+    /**
+     * @return the channelCountMode
+     */
+    public String getChannelCountMode() {
+        return channelCountMode;
+    }
+
+    /**
+     * @param channelCountMode the channelCountMode to set
+     */
+    public void setChannelCountMode(String channelCountMode) {
+        this.channelCountMode = channelCountMode;
+    }
+
+    /**
+     * @return the channelInterpretation
+     */
+    public String getChannelInterpretation() {
+        return channelInterpretation;
+    }
+
+    /**
+     * @return the gain
+     */
+    public String getGain() {
+        return gain.toString();
+    }
+
+    /**
+     * @return the tailTime
+     */
+    public String getTailTime() {
+        return tailTime.toString();
+    }
+
+    /**
+     * @param newChannelInterpretation the channelInterpretation to set
+     */
+    public void setChannelInterpretation(String newChannelInterpretation) {
+        channelInterpretation = newChannelInterpretation;
+    }
+
+    /**
+     * @param newGain the gain to set
+     */
+    public void setGain(String newGain) {
+        gain = new SFFloat(newGain, null, null);
+    }
+
+    /**
+     * @param newTailTime the tailTime to set
+     */
+    public void setTailTime(String newTailTime) {
+        tailTime =  new SFDouble(newTailTime, 0.0, null);
+    }
+      
+  @Override
+  public String getDefaultContainerField()
+  {
+    return "children";
+  }
+}
