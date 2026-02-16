f4df467691762f15841908f6bcf7fac854a932a9
brutzman <don.brutzman@gmail.com>
2026-02-15 21:20:29 -0800

    adjust class reference

--- X3dSourceFilePalette/src/main/java/org/web3d/x3d/actions/EditInBlenderAction.java
+++ X3dSourceFilePalette/src/main/java/org/web3d/x3d/actions/EditInBlenderAction.java
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 1995-2023 held by the author(s).  All rights reserved.
+Copyright (c) 1995-2026 held by the author(s).  All rights reserved.
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions
 are met:
@@ -90,7 +90,7 @@ public final class EditInBlenderAction extends ViewInBaseAction
   @Override
   public String getName()
   {
-    return NbBundle.getMessage(getClass(), "CTL_EditInBlenderAction");
+    return NbBundle.getMessage(EditInBlenderAction.class, "CTL_EditInBlenderAction");
   }
 
   @Override
